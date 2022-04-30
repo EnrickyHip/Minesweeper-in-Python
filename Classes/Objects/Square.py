c8428ffc997e0d0d0b7e035548e8cf101d8971be
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-04-30 15:05:16 -0300

    creating getter and setters foi the Square class

--- Classes/Objects/Square.py
+++ Classes/Objects/Square.py
@@ -7,7 +7,7 @@ class Square:
     self.x = x #x and y here are the index in Table.squares[]
     self.y = y
 
-    self.opened = False 
+    self.isOpened = False 
     self.isBomb = False
     self.isFlaged = False
     self.neighborBombs = 0
@@ -20,3 +20,42 @@ class Square:
     y = self.y * 32 + 104
 
     self.sreen.blit(self.image, (x, y))
+   
+  #properties 
+
+  @property
+  def isOpened(self):
+    return self.__isOpened
+  
+  @property
+  def isBomb(self):
+    return self.__isBomb
+  
+  @property
+  def isFlaged(self):
+    return self.__isFlaged
+  
+  @property
+  def neighborBombs(self):
+    return self.__neighborBombs
+
+  #setters 
+
+  @isOpened.setter
+  def isOpened(self, value):
+    self.__isOpened = value
+  
+  @isBomb.setter
+  def isBomb(self, value):
+    self.__isBomb = value
+  
+  @isFlaged.setter
+  def isFlaged(self, value):
+    self.__isFlaged = value
+  
+  @neighborBombs.setter
+  def neighborBombs(self, value):
+    self.__neighborBombs = value
+
+
+  
